8d60bddc496d4c494b0998f9b1159f7665abc7f4
Angular Developer <work.bhanurana@gmail.com>
2021-07-22 10:06:32 +0530

    changes
    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from './model/user';
-import { AuthService } from './services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "./model/user";
+import { AuthService } from "./services/auth.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'Vibrant-Assignment';
-  constructor(private router: Router, private authService: AuthService) { }
+  title = "Vibrant-Assignment";
+  user: User;
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    const user: User = this.authService.getuser()
-    if (user != null && user.authenticated) {
-      this.router.navigate(['/list-user']);
+    this.user = this.authService.getuser();
+    if (this.user != null && this.user.authenticated) {
+      this.router.navigate(["/list-user"]);
     } else {
-      this.router.navigate(['/login']);
+      this.router.navigate(["/login"]);
     }
-
+  }
+  logout() {
+    this.authService.logout();
   }
 }
-
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,45 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Observable, BehaviorSubject, from } from 'rxjs';
-import { map } from 'rxjs/operators'
-import { User } from '../model/user';
-import { Router } from '@angular/router';
-import { CommonService } from './common-service.service';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable, BehaviorSubject, from } from "rxjs";
+import { map } from "rxjs/operators";
+import { User } from "../model/user";
+import { Router } from "@angular/router";
+import { CommonService } from "./common-service.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
-
-  constructor(private http: HttpClient, private router: Router, private commonUtil: CommonService) {
-
-  }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private commonUtil: CommonService
+  ) {}
   login(user: User) {
     if (user.username === "User1" && user.password === "test1234") {
       user.authenticated = true;
       this.setUser(user);
-      this.commonUtil.notify('success', 'Login Successfull !!!');
-      this.router.navigateByUrl('list-user');
+      this.commonUtil.notify("success", "Login Successfull !!!");
+      this.router.navigateByUrl("list-user");
     } else {
       user.authenticated = false;
-      this.commonUtil.notify('error', 'Login Failed !!!');
+      this.commonUtil.notify("error", "Login Failed !!!");
     }
     return user;
-
   }
 
   logout() {
     // remove user from local storage and set current user to null
-    localStorage.removeItem('User');
+    localStorage.removeItem("User");
+
+    this.router.navigateByUrl("login");
   }
   setUser(user: User) {
-    localStorage.setItem('User', JSON.stringify(user));
+    localStorage.setItem("User", JSON.stringify(user));
   }
   getuser() {
-    return JSON.parse(localStorage.getItem('User'));
+    return JSON.parse(localStorage.getItem("User"));
   }
-
-
-
 }
